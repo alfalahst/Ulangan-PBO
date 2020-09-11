535829c058b9dff34f3bbf7d035bb5963e769010
Alfalah Sugiarta <70927569+alfalahst@users.noreply.github.com>
2020-09-11 21:16:00 +0700

    Add files via upload

---
+++ ujianPBO.java
@@ -0,0 +1,13 @@
+package StringOOP;
+
+public class ujianPBO {
+	public static void main(String[] args) {
+		
+		char ch1[]= {'A','L','F','A','L','A','H'};
+		char ch2[]= {'S','U','G','I','A','R','T','A'};
+		String s1= new String(ch1);
+		String s2= new String(ch2);
+		System.out.println(s1+"-"+s2);
+	}
+
+}
